faddee6a5ab5cc9915fd72a191c41064f8736011
Unknown <ipaterega@gmail.com>
2018-03-15 04:06:56 +0100

    promisekit

--- AirCondition/Controllers/Details/viewModel/StationAirParamsViewModel.swift
+++ AirCondition/Controllers/Details/viewModel/StationAirParamsViewModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import PromiseKit
 
 class StationAirParamsViewModel: StationAirParamsViewModelProtocol {
     
@@ -16,6 +17,10 @@ class StationAirParamsViewModel: StationAirParamsViewModelProtocol {
     var stationAirParams: String?
     
     let stationAirParamsListCells = Observer([StationAirParamsCell]())
+    
+    var stationDetails:[HSStationDetails]?
+    var sensorParameters:HSSensorsData?
+    
     let httpService: HttpService
     
     init(httpService: HttpService = HttpService()) {
@@ -23,8 +28,39 @@ class StationAirParamsViewModel: StationAirParamsViewModelProtocol {
     }
     
     func onLoad(stationId: Int) {
-        httpService.getStationSensorsData(stationId: String(stationId), completion: { [weak self] result in
+        
+        firstly {
+            getStationsSensorData(stationId)
+            }.then { sensorId in
+                self.getSensorData(sensorId)
+        }
+    }
     
-        })
+   private func getStationsSensorData(_ stationId: Int) -> Promise<Int> {
+        
+        return Promise { data in
+            httpService.getStationSensorsData(stationId: String(describing: stationId),
+                                              completion: { [weak self] result in
+               
+                self?.stationDetails = result
+                let sensorId = result.first?.id //just first parameter
+                data.fulfill(sensorId!)
+            })
+        }
+    }
+    
+   private func getSensorData(_ sensorId: Int) -> Promise<HSSensorsData> {
+        return Promise { data in
+            httpService.getSensorsData(sensorId: String(sensorId), completion: { [weak self] result in
+                self?.sensorParameters = result
+                self?.buildChart(result)
+                data.fulfill(result)
+            })
+        }
+    }
+    
+   private func buildChart(_ data: HSSensorsData) {
+        
     }
 }
+
